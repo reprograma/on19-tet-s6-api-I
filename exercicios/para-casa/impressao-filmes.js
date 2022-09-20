9a2188693e2461a655ae56004e7137e888940889
Bianca Trindade <biancatrindade.mkt@gmail.com>
2022-09-19 23:10:59 -0300

    commit1

---
+++ exercicios/para-casa/impressao-filmes.js
@@ -0,0 +1,7 @@
+//1 [x] Imprimir no terminal os titulos da lista de filmes que está no arquivo filmes.js, contendo a seguinte mensagem: `O titulo do filme é: ` titulos. Entre cada titulo, é necessário que haja uma quebra de linha para a melhor visualização.
+
+const obj = require("./filmes.js")
+
+//obj.forEach((filme) => {
+//console.log(`Os títulos dos filmes listados são: ${filme.title.join("\n")}`)
+//})
