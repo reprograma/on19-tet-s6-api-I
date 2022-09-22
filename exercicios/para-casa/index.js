b7ee7f8b10c716842ac335c58e124c4efd7b6094
Liliane Gomes <lilianemourag@gmail.com>
2022-09-22 15:48:00 -0300

    Adiciona exercício semana 6

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,35 @@
+//exercício 1
+
+console.log("Exercício 1")
+const filmes = require("./filmes.json")
+
+       filmes.forEach(filme => {
+        console.log(`O titulo do filme: ${filme.Title}`)
+    })
+
+
+console.log(`___________________________________________________`)
+    
+//exercício 2
+
+console.log("Exercício 2 ainda não concluído")
+const cores = require("./cores-rgb.json")
+cores.keys
+
+const key = "keys"
+cores [key]
+
+
+for (let index = 0; index < cores.length; index++) {
+	const element = cores[index];
+    console.log(`Cor: ${Object.keys(element)}`)
+}
+
+console.log(`______________________________________________________`)
+
+//exercício 3
+
+console.log("Exercício 3 ainda não concluído")
+
+const data = require("./cidade-e-estados.json")
+
